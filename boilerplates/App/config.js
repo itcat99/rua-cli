b183c3c59492d10083266ef94d7971cf55787bc6
FreMaNgo <boping2010@gmail.com>
2018-05-31 10:11:14 +0800

    -[fixed] fixed template config.js

--- boilerplates/App/config.js
+++ boilerplates/App/config.js
@@ -4,12 +4,12 @@ const path = require('path')
  * global folders path
  */
 
-export const src = path.resolve(__dirname, 'src')
-export const public = path.resolve(__dirname, 'public')
-export const scripts = path.resolve(__dirname, 'scripts')
+const src = path.resolve(__dirname, 'src')
+const public = path.resolve(__dirname, 'public')
+const scripts = path.resolve(__dirname, 'scripts')
 
 /* src folders path */
-export const srcFolders = {
+const srcFolders = {
   components: path.join(src, 'components'),
   containers: path.join(src, 'containers'),
   routes: path.join(src, 'routes'),
@@ -17,13 +17,13 @@ export const srcFolders = {
   utils: path.join(src, 'utils')
 }
 /* src files path */
-export const srcFiles = {
+const srcFiles = {
   entry: path.join(src, 'index.js'),
   router: path.join(src, 'router.js'),
   reducer: path.join(src, 'reducer.js'),
 }
 
-export default {
+module.exports = {
   src,
   public,
   scripts,
